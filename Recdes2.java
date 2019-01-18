8ad267d30338c6f46a925c5258ba0779b8126593
Mohit Hooda <mhooda87@gmail.com>
2019-01-18 21:51:20 +0530

    Recursion Descent 2

---
+++ Recdes2.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+class Recdes2
+{
+	static int ptr;
+	static char[] input;
+	public static void main(String args[])
+	{
+		System.out.println("Input the string to be checked :");
+		String s=new Scanner(System.in).nextLine();
+		input=s.toCharArray();
+		ptr=0;
+		boolean result=S();
+		if(result && ptr==input.length)
+			System.out.println("ACCEPTED :)");
+		else System.out.println("REJECTED :(");
+	}
+
+	static boolean S()
+	{
+		int fallback=ptr;
+		if(input[ptr++]!='b')
+		{
+			ptr=fallback;
+			return false;
+		}
+		if(A()==false)
+		{
+				ptr=fallback;
+				return false;
+		}
+		if(input[ptr++]!='c')
+		{
+				ptr=fallback;
+				return false;
+		}
+		return true;
+
+	}
+
+	static boolean A()
+	{
+		int fallback=ptr;
+		if(input[ptr]=='r'&& input[ptr+1]!='e')
+			{
+				ptr++;
+				return true;
+			}
+		else
+		{
+			if(input[ptr++]!='r')
+				{
+					ptr=fallback;
+					return false;
+				}
+			if(input[ptr++]!='e')
+				{
+					ptr=fallback;
+					return false;
+				}
+				return true;
+		}
+	}
+}
+
+
+//S->bAc 
+//A->re|r 
