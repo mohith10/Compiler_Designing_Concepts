5a0397027a02e24ede631faaffd9d36fe3568dfd
Mohit Hooda <mhooda87@gmail.com>
2019-01-18 21:50:33 +0530

    Recursion Descent Program

---
+++ RecDes.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+class RecDes
+{
+	static int ptr;
+	static char[] input;
+	public static void main(String args[])
+	{
+		System.out.println("Input the string to be checked :");
+		String s=new Scanner(System.in).nextLine();
+		input=s.toCharArray();
+		ptr=0;
+		boolean result=S();
+		if(result &&  ptr==input.length)
+		{
+			System.out.println("ACCEPTED :)");
+		}
+		else 
+		{
+				System.out.println("REJECTED :( ");
+		}
+	}
+
+static boolean S()
+{
+	int fallback=ptr;
+	if(input[ptr++]!='c')
+		{
+			ptr=fallback;
+			return false;
+		}
+	if(A()==false)
+		{
+				ptr=fallback;
+				return false;
+		}
+	if(input[ptr++]!='d')
+	{
+		ptr=fallback;
+		return false;
+	}
+	return true;
+}
+static boolean A()
+{
+	int fallback=ptr;
+	if(input[ptr]=='d')
+		{
+			ptr++;
+			return true;
+		}
+
+else
+	{
+		if(input[ptr++]!='a')
+		{
+			ptr=fallback;
+			return false;
+		}
+		if(input[ptr++]!='b')
+		{
+			ptr=fallback;
+			return false;
+		}
+
+		return true;
+	}
+	}
+
+}
+//S->cAd 
+//A->ab|d 
+
+//E -> x + T
+//	T -> (E)
+	//T -> x
+//http://www.pracspedia.com/SPCC/recursivedescentparser.html
